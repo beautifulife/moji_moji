398de5c1b8700c5152e289bc5f92c53ec2d97531
Beautifulife <davidkim1018@gmail.com>
2019-04-08 12:05:21 +0900

    Add testcase

--- src/client/components/App.test.js
+++ src/client/components/App.test.js
@@ -4,6 +4,14 @@ import { configure, shallow, mount, render } from 'enzyme';
 import * as tfc from '@tensorflow/tfjs-core';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import Intro from './Intro';
+import StatusBar from './StatusBar';
+import Camera from './Camera';
+import Countdown from './Countdown';
+import Loader from './Loader';
+import PopUpMenu from './PopUpMenu';
+import Finish from './Finish';
+import Watch from './Watch';
 
 import * as constants from '../utils/constants';
 import * as game from '../utils/game';
@@ -12,15 +20,90 @@ configure({ adapter: new Adapter() });
 jest.mock('@tensorflow/tfjs-core', () => {});
 
 describe('App', () => {
-  const wrapper = shallow(<App />);
+  describe('about renders', () => {
+    let wrapper;
+    const props = {
+      emojiMobileNet: jest.fn()
+    };
 
-  it('component rendered', () => {
-    expect(wrapper.find('.App').length).toBe(1);
+    beforeEach(() => {
+      wrapper = shallow(<App {...props} />);
+    });
+
+    it('component rendered', () => {
+      expect(wrapper.find('.App').length).toBe(1);
+    });
+
+    describe('currentPage is intro', () => {
+      it('intro component rendered', () => {
+        expect(wrapper.find(Intro).length).toBe(1);
+      });
+    });
+
+    describe('currentPage is game', () => {
+      it('Camera component rendered', () => {
+        wrapper.setState({ currentPage: 'game' });
+        expect(wrapper.find(Camera).length).toBe(1);
+      });
+
+      it('StatusBar component rendered when currentEmoji exist', () => {
+        wrapper.setState({
+          currentPage: 'game',
+          currentEmoji: { test: 'test' }
+        });
+        expect(wrapper.find(StatusBar).length).toBe(1);
+      });
+
+      it('Countdown component rendered when isCountdown true', () => {
+        wrapper.setState({
+          currentPage: 'game',
+          isCountdown: true
+        });
+        expect(wrapper.find(Countdown).length).toBe(1);
+      });
+
+      it('PopUpMenu component rendered when didUserFound true', () => {
+        wrapper.setState({
+          currentPage: 'game',
+          didUserFound: true
+        });
+        expect(wrapper.find(PopUpMenu).length).toBe(1);
+      });
+    });
+
+    describe('currentPage is finish', () => {
+      it('Finish component rendered', () => {
+        wrapper.setState({
+          currentPage: 'finish',
+          didUserFound: true
+        });
+        expect(wrapper.find(Finish).length).toBe(1);
+      });
+    });
+
+    describe('currentPage is watch', () => {
+      it('Watch component rendered', () => {
+        wrapper.setState({
+          currentPage: 'watch',
+          didUserFound: true
+        });
+        expect(wrapper.find(Watch).length).toBe(1);
+      });
+    });
+
+    describe('isLoading is ture', () => {
+      it('Loader component rendered', () => {
+        wrapper.setState({
+          isLoading: true
+        });
+        expect(wrapper.find(Loader).length).toBe(1);
+      });
+    });
   });
 
   describe('about state', () => {
     const wrapper = shallow(<App />);
-    
+
     it('initial state set', () => {
       expect(wrapper.state().countdown).toBe(3);
       expect(wrapper.state().currentEmoji).toEqual({});
@@ -34,6 +117,44 @@ describe('App', () => {
       expect(wrapper.state().isLoading).toBe(false);
       expect(wrapper.state().score).toBe(0);
       expect(wrapper.state().timer).toBe(constants.GAME_START_TIME);
+    });
+  });
+
+  describe('about props', () => {
+    const props = {
+      emojiMobileNet: {}
+    };
+    const wrapper = mount(<App {...props} />);
+
+    it('allows to set props', () => {
+      expect(wrapper.props().emojiMobileNet).toBe(props.emojiMobileNet);
+    });
+  });
+
+  describe('about prototype method', () => {
+    let wrapper;
+    let spy;
+    const props = {
+      emojiMobileNet: {
+        load: jest.fn()
+      }
+    };
+
+    beforeEach(() => {
+      wrapper = mount(<App {...props} />);
+    });
+
+    afterEach(() => {
+      spy.mockClear();
+    });
+
+    it('ComponentDidMout called once', () => {
+      const instance = wrapper.instance();
+      instance.createSignalingChannel = jest.fn();
+      spy = jest.spyOn(instance, 'createSignalingChannel');
+
+      instance.componentDidMount();
+      expect(instance.createSignalingChannel).toHaveBeenCalledTimes(1);
     });
   });
 });
--- src/client/components/Camera.test.js
+++ src/client/components/Camera.test.js
@@ -1,25 +1,38 @@
 import '@babel/polyfill';
 import React from 'react';
-import { configure, shallow, mount, render } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Camera from './Camera';
-
-import { VIDEO_PIXELS } from '../utils/constants';
 
 configure({ adapter: new Adapter() });
 
 describe('<Camera />', () => {
   describe('about renders', () => {
-    it('component rendered', () => {
-      const wrapper = shallow(<Camera />);
+    let wrapper;
+    const props = {
+      isGamePaused: false,
+      doSnapShotNow: false,
+      onLoadedMetadata: jest.fn()
+    };
 
+    beforeEach(() => {
+      wrapper = shallow(<Camera {...props} />);
+    });
+
+    it('component rendered', () => {
       expect(wrapper.find('.Camera').length).toBe(1);
     });
 
     it('renders an `.Camera__video`', () => {
-      const wrapper = shallow(<Camera />);
-
       expect(wrapper.find('.Camera__video').length).toBe(1);
+    });
+
+    it('renders an `Camera__capture-flash` if camera paused', () => {
+      const instance = wrapper.instance();
+      instance.unPauseCamera = jest.fn();
+
+      wrapper.setState({ isCameraPaused: true });
+      expect(wrapper.find('.Camera__capture-flash').length).toBe(1);
     });
   });
 
@@ -42,7 +55,7 @@ describe('<Camera />', () => {
       />
     );
 
-    it('allows us to set props', () => {
+    it('allows to set props', () => {
       expect(wrapper.props().isGamePaused).toBe(false);
       expect(wrapper.props().doSnapShotNow).toBe(false);
       expect(wrapper.props().onLoadedMetadata).toBe(onLoadedMetadataMock);
@@ -59,7 +72,7 @@ describe('<Camera />', () => {
     };
 
     beforeEach(() => {
-      wrapper = mount(<Camera {...props} />);
+      wrapper = shallow(<Camera {...props} />);
     });
 
     afterEach(() => {
@@ -77,16 +90,25 @@ describe('<Camera />', () => {
     it('ComponentDidUpdate called after props changed', () => {
       const instance = wrapper.instance();
       instance.pauseCamera = jest.fn();
-      instance.unpauseCamera = jest.fn();
+      instance.unPauseCamera = jest.fn();
       spy = jest.spyOn(instance, 'pauseCamera');
-      spy = jest.spyOn(instance, 'unpauseCamera');
+      spy = jest.spyOn(instance, 'unPauseCamera');
 
       wrapper.setProps({ isGamePaused: true });
       expect(instance.pauseCamera).toHaveBeenCalled();
 
       wrapper.setState({ isCameraPaused: true });
       wrapper.setProps({ isGamePaused: false });
-      expect(instance.unpauseCamera).toHaveBeenCalled();
+      expect(instance.unPauseCamera).toHaveBeenCalled();
+    });
+
+    it('Call snapshot function when doSnapShotNow-prop is true', () => {
+      const instance = wrapper.instance();
+      instance.snapshot = jest.fn();
+      spy = jest.spyOn(instance, 'snapshot');
+
+      wrapper.setProps({ doSnapShotNow: true });
+      expect(instance.snapshot).toHaveBeenCalled();
     });
   });
 });
